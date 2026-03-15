ddf8bb35771c876ffe507f4f154b99743cae50ba
bizops-ops <bizops@zfwhospitality.com>
2026-03-15 21:42:30 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,8 +1,8 @@
-import html from "./Index.html";
-
 export default {
   async fetch(request) {
-    return new Response(html, {
+    const html = await fetch("https://raw.githubusercontent.com/bizops-ops/CCTV/main/Index.html");
+    const text = await html.text();
+    return new Response(text, {
       headers: { "Content-Type": "text/html;charset=UTF-8" },
     });
   },
